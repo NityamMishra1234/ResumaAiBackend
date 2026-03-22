1077b94013ea30488bffd3fef12f0549882c32d5
NityamMishra1234 <nityam1111@gmail.com>
2026-03-22 17:19:11 +0530

    Fix cors crror

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
+// cors for the development 
+
+// "http://localhost:9002",
+//   "http://192.168.1.145:9002",
+//   "http://localhost:5173/",
+//   "http://localhost:5173"
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: [
-      "http://localhost:9002",
-      "http://192.168.1.145:9002",
-      "http://localhost:5173/",
-      "http://localhost:5173"
+     " https://resuma-ai-enterprise.vercel.app/",
+     
     ],
     Credential: true,
   })
